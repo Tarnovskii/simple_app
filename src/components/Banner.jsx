6b14cf70e3256a0ca5958229ab6b86dad3bb12aa
Tarnovskii <artem3dgame@gmail.com>
2020-09-27 18:12:20 +0300

    add banner text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,10 @@ import s from '../stylesheets/banner.module.css'
 export const Banner = () => {
     return (
         <section className={s.wrapper}>
-
+            <h1>Lorem Ipsum</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam arcu metus, luctus eget gravida ac,
+                fermentum a purus. Nam in velit vel dui dapibus sodales quis ac mauris.</p>
         </section>
     )
 }
