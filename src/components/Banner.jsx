729739e45704f9bcbe79cb08bd25f602c224dad5
Tarnovskii <artem3dgame@gmail.com>
2020-09-27 17:55:32 +0300

    init banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from '../stylesheets/banner.module.css'
+
+export const Banner = () => {
+    return (
+        <section className={s.wrapper}>
+
+        </section>
+    )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import s from '../stylesheets/header.module.css'
 export const Header = () => {
     return (
         <header className={s.headerWrapper}>
-
+            <h1>Lorem Ipsum</h1>
+            <button>b1</button>
+            <button>b2</button>
         </header>
     )
 }
